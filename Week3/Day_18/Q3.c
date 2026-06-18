88853b72738216d74cb7a720ab2fbeb509d9d37f
swayamgupta2347-coder <swayamgupta2347@gmail.com>
2026-06-18 13:05:47 +0530

    Day_18 uploaded

---
+++ Week3/Day_18/Q3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main() 
+{
+    int n, i, key;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter sorted elements:\n");
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+    int low = 0, high = n - 1, mid;
+
+    while(low <= high) 
+    {
+        mid = (low + high) / 2;
+        if(a[mid] == key) 
+        {
+            printf("Element found at position %d", mid + 1);
+            return 0;
+        }
+        else if(a[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    printf("Element not found");
+    return 0;
+}
